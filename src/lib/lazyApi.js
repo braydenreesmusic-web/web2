9b8d53c4133b8b4e9af14489423eebb006106d67
Brayden <braydenrees@Braydens-MacBook-Air.local>
2025-12-13 01:06:51 -0600

    fix(lazyApi): use explicit named exports so bundlers can statically analyze exports

--- src/lib/lazyApi.js
+++ src/lib/lazyApi.js
@@ -27,11 +27,69 @@ const fns = [
   'updatePresence','subscribeToPresence','subscribeToListeningSession','updateListeningSession','triggerNotification'
 ]
 
-const exportsObj = {}
-for (const n of fns) exportsObj[n] = wrap(n)
+// Export each wrapper as a static named export so bundlers (Rollup/Vite)
+// can statically analyze the module. Dynamic destructuring prevented
+// Rollup from recognizing exported names during build.
+export const getEvents = wrap('getEvents')
+export const createEvent = wrap('createEvent')
+export const deleteEvent = wrap('deleteEvent')
+export const getTasks = wrap('getTasks')
+export const createTask = wrap('createTask')
+export const updateTask = wrap('updateTask')
 
-export default exportsObj
-export const {
+export const getSavingsGoals = wrap('getSavingsGoals')
+export const createSavingsGoal = wrap('createSavingsGoal')
+export const addContribution = wrap('addContribution')
+export const deleteSavingsGoal = wrap('deleteSavingsGoal')
+
+export const getPins = wrap('getPins')
+export const createPin = wrap('createPin')
+export const updateLocationShare = wrap('updateLocationShare')
+
+export const getCheckIns = wrap('getCheckIns')
+export const getNotes = wrap('getNotes')
+export const getMedia = wrap('getMedia')
+export const getPresence = wrap('getPresence')
+export const getRelationshipData = wrap('getRelationshipData')
+
+export const getBookmarks = wrap('getBookmarks')
+export const createBookmark = wrap('createBookmark')
+export const updateBookmark = wrap('updateBookmark')
+export const deleteBookmark = wrap('deleteBookmark')
+export const bulkUpdateBookmarkOrder = wrap('bulkUpdateBookmarkOrder')
+
+export const getProfileById = wrap('getProfileById')
+
+export const uploadMedia = wrap('uploadMedia')
+export const toggleMediaFavorite = wrap('toggleMediaFavorite')
+export const updateMedia = wrap('updateMedia')
+export const createNote = wrap('createNote')
+
+export const sendPartnerRequest = wrap('sendPartnerRequest')
+export const getPartnerRequests = wrap('getPartnerRequests')
+export const acceptPartnerRequest = wrap('acceptPartnerRequest')
+export const rejectPartnerRequest = wrap('rejectPartnerRequest')
+export const subscribeToPartnerRequests = wrap('subscribeToPartnerRequests')
+
+export const getGameEvents = wrap('getGameEvents')
+export const createGameEvent = wrap('createGameEvent')
+export const subscribeToGameEvents = wrap('subscribeToGameEvents')
+
+export const getMusicTracks = wrap('getMusicTracks')
+export const getPlaylists = wrap('getPlaylists')
+export const searchItunesMusic = wrap('searchItunesMusic')
+export const saveMusicTrack = wrap('saveMusicTrack')
+export const createPlaylist = wrap('createPlaylist')
+export const getPlaylistTracks = wrap('getPlaylistTracks')
+export const addTrackToPlaylist = wrap('addTrackToPlaylist')
+
+export const updatePresence = wrap('updatePresence')
+export const subscribeToPresence = wrap('subscribeToPresence')
+export const subscribeToListeningSession = wrap('subscribeToListeningSession')
+export const updateListeningSession = wrap('updateListeningSession')
+export const triggerNotification = wrap('triggerNotification')
+
+const defaultExport = {
   getEvents,createEvent,deleteEvent,getTasks,createTask,updateTask,
   getSavingsGoals,createSavingsGoal,addContribution,deleteSavingsGoal,
   getPins,createPin,updateLocationShare,
@@ -43,4 +101,6 @@ export const {
   getGameEvents,createGameEvent,subscribeToGameEvents,
   getMusicTracks,getPlaylists,searchItunesMusic,saveMusicTrack,createPlaylist,getPlaylistTracks,addTrackToPlaylist,
   updatePresence,subscribeToPresence,subscribeToListeningSession,updateListeningSession,triggerNotification
-} = exportsObj
+}
+
+export default defaultExport
